627374906c2a2de94a2a335dff11c71ae3b4db04
Sergejs Glusnevs <sergejs.glusnevs@yahoo.de>
2017-06-20 18:14:05 +0200

    Further improvements, including NumPy notebppk

---
+++ PythonFürAnfänger/mymod.py
@@ -0,0 +1,13 @@
+
+""" Hallo! Das ist mein erstes Modul.
+    Funktionen:
+      fibo() -- kehrt String 'Fibo' zurueck
+      filo() -- druckt 'Filo'
+"""
+
+
+def fibo():
+  return 'Fibo'
+
+def filo():
+  print ('Filo')
